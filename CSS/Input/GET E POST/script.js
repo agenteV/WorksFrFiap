5b211480c38301adcfaa47fea1a6ec2918de7fb4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-26 20:59:30 -0300

    exs de java

---
+++ CSS/Input/GET E POST/script.js
@@ -0,0 +1,35 @@
+
+document.querySelector('#criarConta').addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    const tbody = document.querySelector('#dadosConta');
+    const campos = [
+        document.querySelector('#usuario'),
+        document.querySelector('#password'),
+        document.querySelector('#email'),
+        document.querySelector('#dataCadastro'),
+        document.querySelector('#tipoConta'),
+    ];
+
+    const tr = document.createElement('tr');
+
+    campos.forEach(campo => {
+        const td = document.createElement('td')
+
+        if(campo.type === 'date'){
+            const dataFormatada = campo.value;
+            const data = new Date(dataFormatada + 'T12:00:00');
+            td.textContent = data.toLocaleDateString('pt-BR');
+        }
+
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    tbody.appendChild(tr);
+
+    this.reset();
+
+
+    
+});
